cc4d6f79dd5763f4873e1e368b5b2dfad2a71db7
Peter Tillemans <pti@melexis.com>
2017-01-01 01:59:01 +0100

    add temperature sampling with tests

--- tests/Temperatures.js
+++ tests/Temperatures.js
@@ -2,57 +2,86 @@ var Temperatures = require('../lib/Temperatures');
 var test = require('tape');
 var Immutable = require('immutable');
 var _ = require('lodash');
+var redux = require('redux');
+var td = require('testdouble');
+var timers = require('testdouble-timers').default;
+
+timers.use(td);
 
 test('action creator for adding temperatures', function(t) {
-    t.plan(2);
-    
-    var expected = 25.0;
-    var action = Temperatures.addTemperature(expected);
-    var actual = action.temperature;
-    
-    t.equal('ADD_TEMPERATURE', action.type);
-    t.equal(actual, expected, "Expected given temperature as payload");
+  t.plan(2);
+
+  var expected = 25.0;
+  var action = Temperatures.addTemperature(expected);
+  var actual = action.temperature;
+
+  t.equal('ADD_TEMPERATURE', action.type);
+  t.equal(actual, expected, "Expected given temperature as payload");
 });
 
 test('reducer returns initialstate at first', function(t) {
-    t.plan(1);
-    
-    var state = Temperatures.temperatureReducer(undefined, {});
-    var actual = state.get('temperatureList');
-    var expected = Immutable.List([]);
-    t.equal(actual, expected, "state should be initialized when no state is passed");
-    
+  t.plan(1);
+
+  var state = Temperatures.temperatureReducer(undefined, {});
+  var actual = state.get('temperatureList');
+  var expected = Immutable.List([]);
+  t.equal(actual, expected, "state should be initialized when no state is passed");
 });
 
 test('reducer adds temperature to back of list', function(t) {
-    t.plan(2);
-    
-    var state = Immutable.fromJS({
-        temperatureList: [0.0, 0.0, 0.0]
-    })
-    
-    var expected = 25.0;
-    var action = Temperatures.addTemperature(expected);
-    var newState = Temperatures.temperatureReducer(state, action);
-    var actual = newState.get('temperatureList');
-    
-    t.equal(4, actual.count(), "Expected temperatureList to grow");
-    t.equal(actual.last(), expected, "Expected to have the temperature added at the end of the list");
-    
+  t.plan(2);
+
+  var state = Immutable.fromJS({
+    temperatureList: [0.0, 0.0, 0.0]
+  });
+
+  var expected = 25.0;
+  var action = Temperatures.addTemperature(expected);
+  var newState = Temperatures.temperatureReducer(state, action);
+  var actual = newState.get('temperatureList');
+
+  t.equal(4, actual.count(), "Expected temperatureList to grow");
+  t.equal(actual.last(), expected, "Expected to have the temperature at the end of the list");
+
 });
 
 
 test('reducer only keeps 24h worth of temperatures', function(t) {
-    t.plan(1);
-    
-    var nPoints = Temperatures.maxMeasures();
-    
-    var state = undefined;
-    
-    var readings = _.times(nPoints + 1, _.constant(20.0) );
-    var actual = _(readings)
-        .map(Temperatures.addTemperature)
-        .reduce(Temperatures.temperatureReducer, state);
+  t.plan(1);
 
-    t.equal(actual.get('temperatureList').count(), nPoints);
+  var nPoints = Temperatures.maxMeasures();
+
+  var state = undefined;
+
+  var readings = _.times(nPoints + 1, _.constant(20.0) );
+  var actual = _(readings)
+      .map(Temperatures.addTemperature)
+    .reduce(Temperatures.temperatureReducer, state);
+
+  t.equal(actual.get('temperatureList').count(), nPoints);
+});
+
+
+test('sample temperature method measure 12 times per minute', function(t) {
+  t.plan(2);
+
+
+  var measureFunction = function() { return 25.0;  };
+  var store = redux.createStore(Temperatures.temperatureReducer);
+
+  var time = Date.now();
+  var clock = td.timers();
+  clock.tick(time);
+
+  Temperatures.startSampling(store, measureFunction);
+
+  clock.tick(5000);
+  var actualCount = store.getState().get('temperatureList').count();
+  t.equal(actualCount, 1, "expected 1 sample after 6 seconds");
+
+  clock.tick(55000);
+  actualCount = store.getState().get('temperatureList').count();
+  t.equal(actualCount, 12, "expected 12 samples after 61 seconds");
+
+  td.reset();
 });
--- tests/ds1820.js
+++ tests/ds1820.js
@@ -1,30 +1,36 @@
-var test = require('tape')
-var ds1820 = require("../lib/ds1820")
+var test = require('tape');
+var ds1820 = require("../lib/ds1820");
+var td = require('testdouble');
+var Promise = require('bluebird');
+
+td.replace(ds1820, 'readTemperatureSensorFile', function() {
+  return Promise.resolve("blablablablabla\nblablablablbla t=22123");
+});
+
 
 test("read sensor file", function(t) {
-    t.plan(2);
-    
-    ds1820.readTemperatureSensorFile()
-        .then(function(content) {
-            t.equal(content.trim().split('\n').length, 2 , "temperature file should contain 2 lines");
-            t.ok(content.match('t=\\d+'), "temperature file should contain temperature");
-        })
-        .catch(function(err) {
-            t.fail(err);
-        });
-    
+  t.plan(2);
+
+  ds1820.readTemperatureSensorFile()
+    .then(function(content) {
+      t.equal(content.trim().split('\n').length, 2 , "temperature file should contain 2 lines");
+      t.ok(content.match('t=\\d+'), "temperature file should contain temperature");
+    })
+    .catch(function(err) {
+      t.fail(err);
+    });
+
 });
 
 test("read temperature and test if about room temperature", function(t) {
-    t.plan(2);
-    
-    ds1820.readTemperature()
+  t.plan(2);
+
+  ds1820.readTemperature()
     .then(function(temp) {
-        t.ok(temp < 25.0, "Temperature should be lower than 25 degrees, but was " + temp);
-        t.ok(temp > 15.0, "Temperature should be higher than 15 degrees, but was " + temp);
+      t.ok(temp < 25.0, "Temperature should be lower than 25 degrees, but was " + temp);
+      t.ok(temp > 15.0, "Temperature should be higher than 15 degrees, but was " + temp);
     })
     .catch(function(err) {
-        t.fail(err);
-    })
-})
-
+      t.fail(err);
+    });
+});
