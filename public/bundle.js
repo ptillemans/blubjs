0ddcb0cdbbd6a55f405252445d334520f9c0eccd
Demo User <debian@beaglebone.localdomain>
2017-12-20 13:10:20 +0000

    functional target setting

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+console.error("SyntaxError: 'import' and 'export' may only appear at the top level (2:0) while parsing /var/lib/cloud9/projects/blubjs/public/javascript/index.js while parsing file: /var/lib/cloud9/projects/blubjs/public/javascript/index.js");
